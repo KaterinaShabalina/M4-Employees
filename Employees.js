1ab8b828681cfff955fc91dcfedb263cc2da28fc
KaterinaShabalina <e.schabalina@gmail.com>
2023-12-28 08:20:28 +0000

    updated code

--- Employees.js
+++ Employees.js
@@ -13,9 +13,22 @@ const employee2 = {
     companies: ["Meta", "VK"]
 }
 
-// function compareEmployees (employee1, employee2){
-// if 
-
-for(let x in employee1.companies && employee2.companies){
-console.log(employee1.companies[x] && employee2.companies[x]);
+function compareEmployees(employee1, employee2) {
+    if (employee1.companies.length > employee2.companies.length){
+        console.log(employee1.name)
+        for(let i in employee1.companies){
+            console.log( employee1.companies[i])
+        }
+    } else if (employee2.companies.length > employee1.companies.length){
+        console.log(employee2.name)
+        for(let i in employee2.companies){
+            console.log( employee2.companies[i])
+        }
+    } else {
+        console.log(employee1.name)
+        console.log(employee2.name)
+    }
 }
+
+compareEmployees(employee1, employee2)
+
