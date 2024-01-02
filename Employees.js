b6e860ffb34b9c3ed08bd742b3197c54785bc48e
KaterinaShabalina <e.schabalina@gmail.com>
2024-01-02 15:46:16 +0000

    update

--- Employees.js
+++ Employees.js
@@ -13,30 +13,21 @@ const employee2 = {
     companies: ["Meta", "VK"]
 }
 
-// function compareEmployees(employee1, employee2) {
-//     if (employee1.companies.length > employee2.companies.length){
-//         console.log(employee1.name)
-//         for(let i in employee1.companies){
-//             console.log( employee1.companies[i])
-//         }
-//     } else if (employee2.companies.length > employee1.companies.length){
-//         console.log(employee2.name)
-//         for(let i in employee2.companies){
-//             console.log( employee2.companies[i])
-//         }
-//     } else {
-//         console.log(employee1.name)
-//         console.log(employee2.name)
-//     }
-// }
-
-// compareEmployees(employee1, employee2)
-
-
-for(let x in employee1.companies){
-    console.log(employee1.companies[x]);
+function compareEmployees(employee1, employee2) {
+    if (employee1.companies.length > employee2.companies.length){
+        console.log(employee1.name)
+        for(let i in employee1.companies){
+            console.log( employee1.companies[i])
+        }
+    } else if (employee2.companies.length > employee1.companies.length){
+        console.log(employee2.name)
+        for(let i in employee2.companies){
+            console.log( employee2.companies[i])
+        }
+    } else {
+        console.log(employee1.name)
+        console.log(employee2.name)
+    }
 }
 
-for(let x in employee2.companies){
-    console.log(employee2.companies[x]);
-}
+compareEmployees(employee1, employee2)
